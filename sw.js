e0a9ea5ed81995befa5afb0da9b05d544ce03881
AMD <atumahdaniel@gmail.com>
2026-09-20 18:26:40 +0100

    Public proof pages (?u=username) + copy proof link; split app.js; cache v14

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE = 'tvc-v14';
-const ASSETS = ['./', './index.html', './manifest.json'];
+const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
