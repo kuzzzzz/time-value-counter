9ae78ed05ac5ee0dfb8943ac34fb928833a36d2d
AMD <atumahdaniel@gmail.com>
2026-09-20 19:00:51 +0100

    Cache v17 includes app.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'tvc-v16';
+const CACHE = 'tvc-v17';
 const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
