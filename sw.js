0fb945cf6b0f4b466bcf9c05a75614fb6a61aab4
AMD <atumahdaniel@gmail.com>
2026-09-20 18:35:49 +0100

    Cache v15 for proof link deploy

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'tvc-v14';
-const ASSETS = ['./', './index.html', './app.js', './manifest.json'];
+const CACHE = 'tvc-v15';
+const ASSETS = ['./', './index.html', './manifest.json'];
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
 });
